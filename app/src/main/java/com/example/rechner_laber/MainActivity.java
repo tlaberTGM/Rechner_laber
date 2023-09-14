33f8aa089a74b1cfcfe04832fa4ab7abd367f57b
tlaberTGM <tlaber@student.tgm.ac.at>
2023-09-14 15:57:19 +0200

    Berechnen Methode fertiggestellt

--- app/src/main/java/com/example/rechner_laber/MainActivity.java
+++ app/src/main/java/com/example/rechner_laber/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.rechner_laber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void calc(View v) {
+        EditText input1 = findViewById(R.id.editTextNumber2);
+        EditText input2 = findViewById(R.id.editTextNumber3);
+        RadioGroup rg = findViewById(R.id.radioGroup);
+        int zahl1 = Integer.parseInt(input1.getText().toString());
+        int zahl2 = Integer.parseInt(input2.getText().toString());
+        int ergebnis = 0;
+        if(rg.getCheckedRadioButtonId() == R.id.radioButton) {
+            ergebnis = zahl1 + zahl2;
+        } else if(rg.getCheckedRadioButtonId() == R.id.radioButton2){
+            ergebnis = zahl1 - zahl2;
+        } else if(rg.getCheckedRadioButtonId() == R.id.radioButton3) {
+            ergebnis = zahl1 * zahl2;
+        } else if(rg.getCheckedRadioButtonId() == R.id.radioButton4) {
+            ergebnis = zahl1 / zahl2;
+        } else {
+            ergebnis = 0;
+        }
+        TextView anzeige = findViewById(R.id.textView);
+        anzeige.setText(String.valueOf(ergebnis));
+    }
+
+
 }
